31999e0cd4a095c8a6f77b30469927e1decf8cb7
JamieVaughn <wxv6@case.edu>
2022-11-16 01:30:39 -0500

    Make rudimentary attendance calendar

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
-import type { Component } from 'solid-js';
+import type { Component } from "solid-js";
+import { For } from "solid-js";
 
-import logo from './logo.svg';
-import styles from './App.module.css';
+import logo from "./logo.svg";
+import styles from "./App.module.css";
+
+import { students, sectionClassDates, stringDates } from "./data";
 
 const App: Component = () => {
   return (
     <div class={styles.App}>
       <header class={styles.header}>
         <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
+        <span>MCC FE Attendance: Section 1</span>
+        <span>Classes: {stringDates.length}</span>
       </header>
+      <main
+        class={styles.grid}
+        style={`--students: ${students.length}; --classes: ${stringDates.length}`}
+      >
+        <span class={styles.placeholder}>yyyy-mm-dd :: Day</span>
+        <For each={students}>{(student) => <h3>{student}</h3>}</For>
+        <For each={stringDates}>{(item) => <div>{item}</div>}</For>
+        <For
+          each={Array.from({ length: stringDates.length * students.length })}
+        >
+          {(box, index) => (
+            <span>{index() % Math.ceil(Math.random() * 10) ? "✅" : "✖️"}</span>
+          )}
+        </For>
+      </main>
     </div>
   );
 };
