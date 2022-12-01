f0eafdc092f16576beef7f2f759977a59bada02b
JamieVaughn <wxv6@case.edu>
2022-12-01 00:04:35 -0500

    Update absentees

--- src/data/2022_ii.ts
+++ src/data/2022_ii.ts
@@ -10,6 +10,7 @@ const students: StudentsType = [
   // createStudent('Brett', 'bkabat35@gmail.com', 'fcc321f603b-a7f6-4e89-ab59-a2d37a7cfbc3'),
   createStudent('Akul', 'akulsingh1511@gmail.com', 'fccbba861c9-ebd9-497e-8b4b-6b9bcd273f22'),
   createStudent('David Miner', 'dminerur@outlook.com', 'seaeagles'),
+  createStudent('Koi', 'mkareyne@gmail.com', 'k'),
   createStudent('Heather', 'hrcurran13@gmail.com', 'heatherrooo'),
   createStudent('Jackie', 'jsmartin09@gmail.com', 'JSM-Dev'),
   createStudent('Lance', 'lbecoats63@gmail.com', 'fcc7831610a-2fb3-47bb-96a5-602e267ef757'),
@@ -18,7 +19,7 @@ const students: StudentsType = [
 
 const absent: AbsentType =[
   ['akul'],
-  [],
+  ['akul', 'koi'],
   [],
   [],
   [],
